9ff163c6b9c643d74d9ac3a2569ea3f93d36d25e
Oleksii Puzanov <alexey.puzanov@i.ua>
2023-04-08 20:24:19 +0300

    Add statistics

---
+++ src/utils/utils.jsx
@@ -0,0 +1,7 @@
+export const getRandomHexColor = () => {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+export const getFlexBasis = ({length}) => {
+  return `calc(100%/${length})`;
+}
